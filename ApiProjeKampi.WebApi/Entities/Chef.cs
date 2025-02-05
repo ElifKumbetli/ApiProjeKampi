93500ce2a435f1df9bca8b847fc105edafd052b0
ElifKumbetli <elifkumbetli@gmail.com>
2025-02-05 21:44:48 +0300

    Add project files.

---
+++ ApiProjeKampi.WebApi/Entities/Chef.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiProjeKampi.WebApi.Entities
+{
+    public class Chef
+    {
+        public int ChefId { get; set; }
+
+        public int NameSurname { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+    }
+}
