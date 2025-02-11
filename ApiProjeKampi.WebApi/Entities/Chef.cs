1f83f86f864a13cac2a233094814f4e0e91d4d26
ElifKumbetli <elifkumbetli@gmail.com>
2025-02-11 18:38:56 +0300

    Swagger Aracı, DI ve Kategori Ekleme İşlemi Kategori Api İşlemlerinin Tamamlanması Chef Api İşlemleri

--- ApiProjeKampi.WebApi/Entities/Chef.cs
+++ ApiProjeKampi.WebApi/Entities/Chef.cs
@@ -4,7 +4,7 @@
     {
         public int ChefId { get; set; }
 
-        public int NameSurname { get; set; }
+        public string NameSurname { get; set; }
 
         public string Title { get; set; }
 
---
+++ ApiProjeKampi.WebApi/Migrations/20250211153221_UpdateNameSurnameToString.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiProjeKampi.WebApi.Migrations
+{
+    public partial class UpdateNameSurnameToString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NameSurname",
+                table: "Chefs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NameSurname",
+                table: "Chefs",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
